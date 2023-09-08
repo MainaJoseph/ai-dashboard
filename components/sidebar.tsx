10df511338d5d3f24039675c577a16e5b7e67858
MainaJoseph <mainajm254@gmail.com>
2023-09-08 09:38:49 +0300

    Added moserrat font to logo text Nova

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,13 +2,21 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { Montserrat } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const monserrat = Montserrat ({
+    weight: "600",
+     subsets: ["latin"]
+     });
 
 const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white" >
     <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-        <div className="relative w-20 h-20 mr-4">
+        <div className="relative w-10 h-10 mr-4">
 
             <Image
             fill
@@ -17,6 +25,9 @@ const Sidebar = () => {
             />
 
         </div>
+        <h1 className={cn("text-2xl font-bold", monserrat.className)}>
+            Nova
+        </h1>
         </Link>
     </div>
     </div>
