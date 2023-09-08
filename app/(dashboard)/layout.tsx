c502c46ce00ffaadd8d6de306274fa5bb626c795
MainaJoseph <mainajm254@gmail.com>
2023-09-08 08:27:27 +0300

    formated the sidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 
 
 const DashboardLayout = async ({
@@ -9,9 +10,14 @@ const DashboardLayout = async ({
   return ( 
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        
+        <div>
+            Hello sidebar
+        </div>
       </div>
-      Hello SideBar
+      <main className="md:pl-72 pb-10">
+          <Navbar />
+        {children}
+      </main>
     </div>
    );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'; // Import React
+import { Button } from '@/components/ui/button'; // Assuming this is the correct path
+import { Menu } from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center p-4">
+      <Button variant="ghost" size="icon" className="md:hidden"> 
+        <Menu />
+      </Button>
+      <div className='flex w-full justify-end'>
+<UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
