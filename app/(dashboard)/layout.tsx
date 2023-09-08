88d2076bff2a9ee927493f0ddb3231282cbdceb3
MainaJoseph <mainajm254@gmail.com>
2023-09-08 09:26:33 +0300

    Added a logo to sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 
 const DashboardLayout = async ({
@@ -10,9 +11,7 @@ const DashboardLayout = async ({
   return ( 
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <div>
-            Hello sidebar
-        </div>
+        <Sidebar />
       </div>
       <main className="md:pl-72 pb-10">
           <Navbar />
